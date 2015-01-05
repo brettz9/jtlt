fd17b7f5d838a7b7559e13ee8a3d076e56c72860
Brett Zamir <brettz9@yahoo.com>
2015-01-05 12:49:53 -0700

    rely on native if may become available

--- index.js
+++ index.js
@@ -410,7 +410,7 @@ JTLT.prototype.transform = function (mode) {
 
 
 if (exports !== undefined) {
-    Object.assign = require('object-assign');
+    Object.assign = Object.assign || require('object-assign');
     exports.JTLT = JTLT;
     exports.JSONPathTransformer = JSONPathTransformer;
     exports.JSONPathTransformerContext = JSONPathTransformerContext;
