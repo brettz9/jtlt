3096dabf691e41fc178c8e9d182e168146edd42c
Brett Zamir <brettz9@yahoo.com>
2015-01-09 15:39:16 -0700

    Begin config testing

---
+++ test/test.JTLT-config.js
@@ -0,0 +1,67 @@
+/*global require, module*/
+/*jslint vars:true*/
+(function () {'use strict';
+
+var JTLT = require('../').JTLT,
+    testCase = require('nodeunit').testCase;
+
+var json = {
+  "store": {
+    "book": [
+      {
+        "category": "reference",
+        "author": "Nigel Rees",
+        "title": "Sayings of the Century",
+        "price": 8.95
+      },
+      {
+        "category": "fiction",
+        "author": "Evelyn Waugh",
+        "title": "Sword of Honour",
+        "price": 12.99
+      },
+      {
+        "category": "fiction",
+        "author": "Herman Melville",
+        "title": "Moby Dick",
+        "isbn": "0-553-21311-3",
+        "price": 8.99
+      },
+      {
+        "category": "fiction",
+        "author": "J. R. R. Tolkien",
+        "title": "The Lord of the Rings",
+        "isbn": "0-395-19395-8",
+        "price": 22.99
+      }
+    ],
+    "bicycle": {
+      "color": "red",
+      "price": 19.95
+    }
+  }
+};
+
+module.exports = testCase({
+    'ajaxData': function (test) {
+        test.expect(1);
+        var expected = json;
+        
+        var a = JTLT({
+            templates: [{
+                name: 'author', // For use with calling templates
+                path: '$..author',
+                template: function () {
+                    return '<b>' + this.valueOf({select: '.'}) + '</b>';
+                }
+            }],
+            ajaxData: '../examples/data/jsonpath-sample.json',
+            success: function (result) {
+                test.deepEqual(expected, this.data);
+                test.done();
+            }
+        });
+    }
+});
+
+}());
---
+++ test/test.basic.js
@@ -0,0 +1,50 @@
+/*global require, module*/
+/*jslint vars:true*/
+(function () {'use strict';
+
+var JTLT = require('../').JTLT,
+    testCase = require('nodeunit').testCase;
+
+var json = {
+    "store": {
+        "book": { "category": "reference",
+            "author": "Nigel Rees",
+            "title": "Sayings of the Century",
+            "price": [8.95, 8.94, 8.93]
+        },
+        "books": [
+            { "category": "reference",
+                "author": "Nigel Rees",
+                "title": "Sayings of the Century",
+                "price": [8.95, 8.94, 8.93]
+            }
+        ]
+    }
+};
+
+module.exports = testCase({
+    'basic test': function (test) {
+        test.expect(1);
+
+        var expected = '';
+        var result = '';
+        
+        var a = JTLT({
+            templates: [{
+                name: 'author', // For use with calling templates
+                path: '$..author',
+                template: function () {
+                    return '<b>' + this.valueOf({select: '.'}) + '</b>';
+                }
+            }],
+            ajaxData: '../examples/data/jsonpath-sample.json',
+            success: function (result) {
+                console.log('result:'+result);
+            }
+        });
+        test.deepEqual(expected, result);
+        test.done();
+    }
+});
+
+}());
