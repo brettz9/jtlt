c7beccafe4a412194c1dcb6963afa5b65abdca68
Brett Zamir <brettz9@yahoo.com>
2016-01-08 19:25:45 -0700

    Update bundled get-json

--- bower_components/get-json/README.md
+++ bower_components/get-json/README.md
@@ -2,11 +2,37 @@
 
 getJSON function similar to that of jQuery's.
 
-```javascript
+```js
 getJSON(url, function (data) {
     // Do something with "data"
 });
 ```
+
+An optional third argument can be provided as an error-back (which will
+be supplied the error message and originally supplied URL).
+
+Also accepts an array of URLs (waiting for all to load):
+
+```js
+getJSON([url1, ur2], function (obj1, obj2) {
+    // Do something with "obj1" and "obj2"
+});
+```
+
+If no callback or error-back is provided (or when an array of URLs is
+supplied), a promise will be returned. If an array is provided along with
+a callback, that callback will be executed before the promise is resolved.
+
+You can thus use Promises as follows:
+
+```js
+getJSON([url1, ur2]).then(function (objs) {
+    // Do something with "objs" array
+}, function (err) {
+    // Handle any errors here
+});
+```
+
 
 # Install
 
@@ -24,4 +50,6 @@ npm install simple-get-json
 - Support named parameters ala jQuery
 - Support rest of jQuery API
 
+# Notes
 
+See also [postJSON](https://github.com/brettz9/postJSON).
--- bower_components/get-json/index.js
+++ bower_components/get-json/index.js
@@ -1,10 +1,32 @@
-/*global require, module*/
 /*jslint vars: true*/
+
+var require, module;
+
 (function () {'use strict';
 
-function getJSON (jsonURL, cb) {
+function getJSON (jsonURL, cb, errBack) {
+    function handleError (e) {
+        if (errBack) {
+            return errBack(e, jsonURL);
+        }
+        throw e + ' (' + jsonURL + ')';
+    }
+    if (Array.isArray(jsonURL)) {
+        return Promise.all(jsonURL.map(getJSON)).then(function (arr) {
+            if (cb) {
+                cb.apply(null, arr);
+            }
+            return arr;
+        }).catch(function (err) {
+            handleError(err);
+        });
+    }
+    if (typeof cb !== 'function' && typeof errBack !== 'function') { // Do typeof checks to allow for easier array promise usage of getJSON (as above)
+        return new Promise(getJSON.bind(null, jsonURL));
+    }
     try {
-        var r = typeof require === 'undefined' ? new XMLHttpRequest() : new (require('local-xmlhttprequest').XMLHttpRequest);
+        // Todo: use fetch API for greater elegance
+        var r = require === undefined ? new XMLHttpRequest() : new (require('local-xmlhttprequest').XMLHttpRequest)();
 
         r.open('GET', jsonURL, true);
         //r.responseType = 'json';
@@ -13,7 +35,7 @@ function getJSON (jsonURL, cb) {
             if (r.status === 200) {
                 //var json = r.json;
                 var response = r.responseText;
-                
+
                 var json = JSON.parse(response);
                 cb(json);
                 return;
@@ -24,11 +46,11 @@ function getJSON (jsonURL, cb) {
         r.send();
     }
     catch (e) {
-        throw e + ' (' + jsonURL + ')';
+        handleError(e);
     }
 }
 
-if (typeof module !== 'undefined') {
+if (module !== undefined) {
     module.exports = getJSON;
 }
 else {
