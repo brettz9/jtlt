fb7c2795b55dc7e8116cb39529a0eaa71951f3d0
Brett Zamir <brettz9@yahoo.com>
2016-01-10 16:31:15 -0700

    simplify test code

--- test/test.basic.js
+++ test/test.basic.js
@@ -39,46 +39,45 @@ var jtltConfig = {
     outputType: 'string'
 };
 
+function runTest (jtltConfig) {
+  try {
+      JTLT(jtltConfig);
+  }
+  catch (e) {
+      alert(e);
+  }
+}
+
+
 testBasic = {
     'basic test': function (t) {
-        test = t;
         // test.expect(1);
+        test = t;
 
         expected = '<b>Nigel Rees</b><b>Evelyn Waugh</b><b>Herman Melville</b><b>J. R. R. Tolkien</b>';
-        try {
-            jtltConfig.templates[1] = {
-                name: 'author', // For use with calling templates
-                path: '$.store.book[*].author',
-                template: function () {
-                    this.string('<b>');
-                    this.valueOf({select: '.'});
-                    this.string('</b>');
-                }
-            };
-            JTLT(jtltConfig);
-        }
-        catch (e) {
-            alert(e);
-        }
+        jtltConfig.templates[1] = {
+            name: 'author', // For use with calling templates
+            path: '$.store.book[*].author',
+            template: function () {
+                this.string('<b>');
+                this.valueOf({select: '.'});
+                this.string('</b>');
+            }
+        };
+        runTest(jtltConfig);
     },
     'should be able to use argument to template': function (t) {
         test = t;
-        // test.expect(1);
 
         expected = '<b>Nigel Rees</b><b>Evelyn Waugh</b><b>Herman Melville</b><b>J. R. R. Tolkien</b>';
-        try {
-            jtltConfig.templates[1] = {
-                name: 'author', // For use with calling templates
-                path: '$.store.book[*].author',
-                template: function (cfg) {
-                    this.string('<b>' + cfg.value + '</b>');
-                }
-            };
-            JTLT(jtltConfig);
-        }
-        catch (e) {
-            alert(e);
-        }
+        jtltConfig.templates[1] = {
+            name: 'author', // For use with calling templates
+            path: '$.store.book[*].author',
+            template: function (cfg) {
+                this.string('<b>' + cfg.value + '</b>');
+            }
+        };
+        runTest(jtltConfig);
     }
 };
 
