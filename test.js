2f7ff6d2be1089df7a3dbb42d2470fc2ad5e3dd2
Brett Zamir <brettz9@yahoo.com>
2026-08-25 20:45:22 -0700

    incomplete work on schemas

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import xsdValidator from 'xsd-validator';
+import {validateXml} from 'xml-xsd-validator-browser';
 
 const xmlDocument = `<?xml version="1.0"?>
 <recipe xmlns="http://www.namespace.org/recipe">
@@ -6,40 +6,14 @@ const xmlDocument = `<?xml version="1.0"?>
     <title>sugar cookies</title>
   </description>
 </recipe>`;
-const xsdSchema = `<?xml version="1.0" encoding="utf-8"?>
-<xsd:schema
-   elementFormDefault="qualified"
-   xmlns="http://www.namespace.org/recipe"
-   targetNamespace="http://www.namespace.org/recipe"
+const xsdSchema = 'http://localhost:8021/schema.xsd';
 
-   version="1.0"
-   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-   xmlns:r="http://www.namespace.org/recipe">
+try {
+  const isValid = await validateXml(xmlDocument, xsdSchema);
 
-  <xsd:element name="recipe">
-    <xsd:complexType>
-      <xsd:choice>
-        <xsd:element name="description" type="descriptionType"
-          minOccurs="1" maxOccurs="1" />
-      </xsd:choice>
-    </xsd:complexType>
-  </xsd:element>
-
-  <xsd:complexType name="descriptionType">
-    <xsd:all>
-      <xsd:element name="title">
-        <xsd:simpleType>
-          <xsd:restriction base="xsd:string">
-            <xsd:minLength value="5" />
-            <xsd:maxLength value="55" />
-          </xsd:restriction>
-        </xsd:simpleType>
-      </xsd:element>
-    </xsd:all>
-  </xsd:complexType>
-</xsd:schema>`;
-
-const isValid = xsdValidator.default(xmlDocument, xsdSchema);
-
-// eslint-disable-next-line no-console -- Testing
-console.log('isValid', isValid);
+  // eslint-disable-next-line no-console -- Testing
+  console.log('isValid', isValid);
+} catch (err) {
+  // eslint-disable-next-line no-console -- Debugging
+  console.log('err', err);
+}
