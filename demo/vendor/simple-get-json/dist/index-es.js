43c1bcd1cc119aad67028b4c8ef3d6b1b7920b49
Brett Zamir <brettz9@yahoo.com>
2026-09-20 21:18:30 -0700

    chore: update simple-get-json

--- demo/vendor/simple-get-json/dist/index-es.js
+++ demo/vendor/simple-get-json/dist/index-es.js
@@ -6,11 +6,144 @@ function _arrayLikeToArray(r, a) {
 function _arrayWithoutHoles(r) {
   if (Array.isArray(r)) return _arrayLikeToArray(r);
 }
+function asyncGeneratorStep(n, t, e, r, o, a, c) {
+  try {
+    var i = n[a](c),
+      u = i.value;
+  } catch (n) {
+    return void e(n);
+  }
+  i.done ? t(u) : Promise.resolve(u).then(r, o);
+}
+function _asyncToGenerator(n) {
+  return function () {
+    var t = this,
+      e = arguments;
+    return new Promise(function (r, o) {
+      var a = n.apply(t, e);
+      function _next(n) {
+        asyncGeneratorStep(a, r, o, _next, _throw, "next", n);
+      }
+      function _throw(n) {
+        asyncGeneratorStep(a, r, o, _next, _throw, "throw", n);
+      }
+      _next(void 0);
+    });
+  };
+}
 function _iterableToArray(r) {
   if ("undefined" != typeof Symbol && null != r[Symbol.iterator] || null != r["@@iterator"]) return Array.from(r);
 }
 function _nonIterableSpread() {
   throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _regenerator() {
+  /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/babel/babel/blob/main/packages/babel-helpers/LICENSE */
+  var e,
+    t,
+    r = "function" == typeof Symbol ? Symbol : {},
+    n = r.iterator || "@@iterator",
+    o = r.toStringTag || "@@toStringTag";
+  function i(r, n, o, i) {
+    var c = n && n.prototype instanceof Generator ? n : Generator,
+      u = Object.create(c.prototype);
+    return _regeneratorDefine(u, "_invoke", function (r, n, o) {
+      var i,
+        c,
+        u,
+        f = 0,
+        p = o || [],
+        y = false,
+        G = {
+          p: 0,
+          n: 0,
+          v: e,
+          a: d,
+          f: d.bind(e, 4),
+          d: function (t, r) {
+            return i = t, c = 0, u = e, G.n = r, a;
+          }
+        };
+      function d(r, n) {
+        for (c = r, u = n, t = 0; !y && f && !o && t < p.length; t++) {
+          var o,
+            i = p[t],
+            d = G.p,
+            l = i[2];
+          r > 3 ? (o = l === n) && (u = i[(c = i[4]) ? 5 : (c = 3, 3)], i[4] = i[5] = e) : i[0] <= d && ((o = r < 2 && d < i[1]) ? (c = 0, G.v = n, G.n = i[1]) : d < l && (o = r < 3 || i[0] > n || n > l) && (i[4] = r, i[5] = n, G.n = l, c = 0));
+        }
+        if (o || r > 1) return a;
+        throw y = true, n;
+      }
+      return function (o, p, l) {
+        if (f > 1) throw TypeError("Generator is already running");
+        for (y && 1 === p && d(p, l), c = p, u = l; (t = c < 2 ? e : u) || !y;) {
+          i || (c ? c < 3 ? (c > 1 && (G.n = -1), d(c, u)) : G.n = u : G.v = u);
+          try {
+            if (f = 2, i) {
+              if (c || (o = "next"), t = i[o]) {
+                if (!(t = t.call(i, u))) throw TypeError("iterator result is not an object");
+                if (!t.done) return t;
+                u = t.value, c < 2 && (c = 0);
+              } else 1 === c && (t = i.return) && t.call(i), c < 2 && (u = TypeError("The iterator does not provide a '" + o + "' method"), c = 1);
+              i = e;
+            } else if ((t = (y = G.n < 0) ? u : r.call(n, G)) !== a) break;
+          } catch (t) {
+            i = e, c = 1, u = t;
+          } finally {
+            f = 1;
+          }
+        }
+        return {
+          value: t,
+          done: y
+        };
+      };
+    }(r, o, i), true), u;
+  }
+  var a = {};
+  function Generator() {}
+  function GeneratorFunction() {}
+  function GeneratorFunctionPrototype() {}
+  t = Object.getPrototypeOf;
+  var c = [][n] ? t(t([][n]())) : (_regeneratorDefine(t = {}, n, function () {
+      return this;
+    }), t),
+    u = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(c);
+  function f(e) {
+    return Object.setPrototypeOf ? Object.setPrototypeOf(e, GeneratorFunctionPrototype) : (e.__proto__ = GeneratorFunctionPrototype, _regeneratorDefine(e, o, "GeneratorFunction")), e.prototype = Object.create(u), e;
+  }
+  return GeneratorFunction.prototype = GeneratorFunctionPrototype, _regeneratorDefine(u, "constructor", GeneratorFunctionPrototype), _regeneratorDefine(GeneratorFunctionPrototype, "constructor", GeneratorFunction), GeneratorFunction.displayName = "GeneratorFunction", _regeneratorDefine(GeneratorFunctionPrototype, o, "GeneratorFunction"), _regeneratorDefine(u), _regeneratorDefine(u, o, "Generator"), _regeneratorDefine(u, n, function () {
+    return this;
+  }), _regeneratorDefine(u, "toString", function () {
+    return "[object Generator]";
+  }), (_regenerator = function () {
+    return {
+      w: i,
+      m: f
+    };
+  })();
+}
+function _regeneratorDefine(e, r, n, t) {
+  var i = Object.defineProperty;
+  try {
+    i({}, "", {});
+  } catch (e) {
+    i = 0;
+  }
+  _regeneratorDefine = function (e, r, n, t) {
+    function o(r, n) {
+      _regeneratorDefine(e, r, function (e) {
+        return this._invoke(r, n, e);
+      });
+    }
+    r ? i ? i(e, r, {
+      value: n,
+      enumerable: !t,
+      configurable: !t,
+      writable: !t
+    }) : e[r] = n : (o("next", 0), o("throw", 1), o("return", 2));
+  }, _regeneratorDefine(e, r, n, t);
 }
 function _toConsumableArray(r) {
   return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread();
@@ -31,17 +164,17 @@ function _unsupportedIterableToArray(r, a) {
  */
 
 /**
-* @callback SimpleJSONCallback
-* @param {...JSONValue} json
-* @returns {void}
-*/
+ * @callback SimpleJSONCallback
+ * @param {...JSONValue[]} json
+ * @returns {void}
+ */
 
 /**
-* @callback SimpleJSONErrback
-* @param {Error} err
-* @param {string|string[]} jsonURL
-* @returns {JSONValue}
-*/
+ * @callback SimpleJSONErrback
+ * @param {Error} err
+ * @param {string|string[]} jsonURL
+ * @returns {JSONValue}
+ */
 
 /**
  * @typedef {((
@@ -60,88 +193,76 @@ function _unsupportedIterableToArray(r, a) {
  * @param {import('./index-polyglot.js').SimpleFetch} [cfg.fetch]
  * @returns {getJSONCallback}
  */
-
-function _await(value, then, direct) {
-  if (!value || !value.then) {
-    value = Promise.resolve(value);
-  }
-  return then ? value.then(then) : value;
-}
-function _invoke(body, then) {
-  var result = body();
-  if (result && result.then) {
-    return result.then(then);
-  }
-  return then(result);
-}
-function _catch(body, recover) {
-  try {
-    var result = body();
-  } catch (e) {
-    return recover(e);
-  }
-  if (result && result.then) {
-    return result.then(void 0, recover);
-  }
-  return result;
-}
 function buildGetJSONWithFetch() {
   var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
     _ref$fetch = _ref.fetch,
     fetch = _ref$fetch === void 0 ? typeof window !== 'undefined' ? window.fetch : self.fetch : _ref$fetch;
   /**
-  * @type {getJSONCallback}
-  */
-  return function getJSON(jsonURL, cb, errBack) {
-    try {
-      var _exit = false;
-      return _await(_catch(function () {
-        return _invoke(function () {
-          if (Array.isArray(jsonURL)) {
-            return _await(Promise.all(jsonURL.map(function (url) {
+   * @type {getJSONCallback}
+   */
+  return /*#__PURE__*/function () {
+    var _getJSON = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee(jsonURL, cb, errBack) {
+      var arrResult, resp, result, e, _t;
+      return _regenerator().w(function (_context) {
+        while (1) switch (_context.p = _context.n) {
+          case 0:
+            _context.p = 0;
+            if (!Array.isArray(jsonURL)) {
+              _context.n = 2;
+              break;
+            }
+            _context.n = 1;
+            return Promise.all(jsonURL.map(function (url) {
               return /** @type {getJSONCallback} */getJSON(url);
-            })), function (arrResult) {
-              if (cb) {
-                // eslint-disable-next-line promise/prefer-await-to-callbacks -- Old-style API
-                cb.apply(void 0, _toConsumableArray(arrResult));
-              }
-              _exit = true;
-              return arrResult;
-            });
-          }
-        }, function (_result) {
-          return _exit ? _result : _await(fetch(jsonURL), function (resp) {
-            return _await(resp.json(), function (result) {
-              return typeof cb === 'function'
+            }));
+          case 1:
+            arrResult = _context.v;
+            if (cb) {
               // eslint-disable-next-line promise/prefer-await-to-callbacks -- Old-style API
-              ? cb(result) : result;
-              // https://github.com/bcoe/c8/issues/135
-              /* c8 ignore next */
-            });
-          });
-        });
-      }, function (err) {
-        var e = /** @type {Error} */err;
-        e.message += " (File: ".concat(jsonURL, ")");
-        if (errBack) {
-          return errBack(e, jsonURL);
+              cb.apply(void 0, _toConsumableArray(arrResult));
+            }
+            return _context.a(2, arrResult);
+          case 2:
+            _context.n = 3;
+            return fetch(jsonURL);
+          case 3:
+            resp = _context.v;
+            _context.n = 4;
+            return resp.json();
+          case 4:
+            result = _context.v;
+            return _context.a(2, typeof cb === 'function'
+            // eslint-disable-next-line promise/prefer-await-to-callbacks -- Old-style API
+            ? cb(result) : result);
+          case 5:
+            _context.p = 5;
+            _t = _context.v;
+            e = /** @type {Error} */_t;
+            e.message += " (File: ".concat(jsonURL, ")");
+            if (!errBack) {
+              _context.n = 6;
+              break;
+            }
+            return _context.a(2, errBack(e, jsonURL));
+          case 6:
+            throw e;
+          case 7:
+            return _context.a(2);
         }
-        throw e;
-        // https://github.com/bcoe/c8/issues/135
-        /* c8 ignore next */
-      }));
-      /* c8 ignore next */
-    } catch (e) {
-      return Promise.reject(e);
+      }, _callee, null, [[0, 5]]);
+    }));
+    function getJSON(_x, _x2, _x3) {
+      return _getJSON.apply(this, arguments);
     }
-  };
+    return getJSON;
+  }();
 }
 
 var getJSON = buildGetJSONWithFetch();
 
 /**
  * For polymorphism with Node.
- * @returns {getJSON}
+ * @returns {typeof getJSON}
  */
 var buildGetJSON = function buildGetJSON() {
   return getJSON;
